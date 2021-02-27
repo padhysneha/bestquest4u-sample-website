68146f238e7253b9d2b533e2c259445d1356b25e
padhysneha <padhysneha@users.noreply.github.com>
2021-02-27 08:58:01 +0530

    This is my first commit

--- udemy.sql
+++ udemy.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `course_info`
 --
 
-DROP TABLE IF EXISTS `course_info`;
+DROP TABLE IF EXISTS `course_info` ;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `course_info` (
